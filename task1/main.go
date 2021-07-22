cc03b4846c075f95b484d4482314426de95f5aa0
x4aekx <4aek20@gmail.com>
2021-07-22 10:40:44 +0300

    tasks 1-4

---
+++ task1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [5]int{76, 77, 78, 79, 80}
+
+	var b []int = a[1:4] // слайс, который ссылается на массив
+
+	b[0] = 1
+
+	b = append(b, 3)
+	b = append(b, 4)
+
+	a[2] = 2
+
+	fmt.Println(b, a)
+}
