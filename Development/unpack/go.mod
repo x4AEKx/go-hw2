15452c1cdbbab1786f21eb6c5ce9ed6b5a2b7fab
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-07-26 22:35:38 +0300

    development task2 - create tests

---
+++ Development/unpack/go.mod
@@ -0,0 +1,3 @@
+module github.com/x4AEKx/go-hw2/tree/master/Development/unpack
+
+go 1.16
---
+++ Development/unpack/unpack.go
@@ -0,0 +1,6 @@
+package unpack
+
+// UnpStr : unpacks the string
+func UnpStr(str string) string {
+	return "test"
+}
