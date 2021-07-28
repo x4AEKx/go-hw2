faebd5583b3272af5f2714295340453dd49c030e
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-07-28 23:41:10 +0300

    development task1

--- Development/task1/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/x4aekx/go-hw2/Development/task1
-
-go 1.16
--- Development/task1/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/x4aekx/go-hw2/Development/task1/ext"
-
-func main() {
-	ext.GetTime("")
-}
