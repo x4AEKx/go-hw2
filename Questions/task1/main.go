e0536449159b246036a8345a368c78e601b06fa6
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-08-12 23:53:53 +0300

    questions task1

--- Questions/task1/main.go
+++ Questions/task1/main.go
@@ -6,15 +6,25 @@ import (
 
 func main() {
 	a := [5]int{76, 77, 78, 79, 80}
-
-	var b []int = a[1:4] // слайс, который ссылается на массив
-
+	var b []int = a[1:4] // B слайс ссылается на массив A
 	b[0] = 1
+	b[1] = 2
+	b[2] = 3
+	fmt.Printf("len a = %d cap a = %d\n", len(a), cap(a))
+	fmt.Printf("len b = %d cap b = %d\n", len(b), cap(b))
+	fmt.Println("a = ", a)
+	fmt.Printf("b = %v\n\n", b)
 
-	b = append(b, 3)
-	b = append(b, 4)
+	b = append(b, 4) // B слайс все ещё ссылается на массив A, т.к. capacity позволяет
+	fmt.Printf("len a = %d cap a = %d\n", len(a), cap(a))
+	fmt.Printf("len b = %d cap b = %d\n", len(b), cap(b))
+	fmt.Println("a = ", a)
+	fmt.Printf("b = %v\n\n", b)
 
-	a[2] = 2
-
-	fmt.Println(b, a)
+	b = append(b, 5) // B не ссылается на массив A, т.к. capacity удвоилось и B ссылается уже на другой массив, отличный от A
+	b[0] = 42
+	fmt.Printf("len a = %d cap a = %d\n", len(a), cap(a))
+	fmt.Printf("len b = %d cap b = %d\n", len(b), cap(b))
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
 }
