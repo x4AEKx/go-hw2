c2afed311e88e02c5c036bd053798e7d38c8662d
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-08-13 00:01:47 +0300

    questions task3

--- Questions/task3/main.go
+++ Questions/task3/main.go
@@ -15,8 +15,10 @@ func main() {
 
 	fmt.Println(err)
 
-	fmt.Println(err == nil) // пустой интерфейс не равен nil, т.к. содержит внутри себя...
+	fmt.Println(err == nil) // пустой интерфейс не равен nil, т.к. содержит внутри себя тип(*os.PathError) и значение(равное nil)
 
-	fmt.Printf("%T\n", err)
+	fmt.Printf("%T\n", err) // тип интерфейса
+	fmt.Printf("%v\n", err) // значение интерфейса
+
 	fmt.Printf("%T\n", nil)
 }
